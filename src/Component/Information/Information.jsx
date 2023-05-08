587d84ebfbc13b7e3c306fc3b8124b4d318eeb14
Victor Parizio <victorparizio@hotmail.com>
2023-05-08 11:22:12 -0300

    feat (Information) Estilos Modal Information Foi feito o css do Modal information.

--- src/Component/Information/Information.jsx
+++ src/Component/Information/Information.jsx
@@ -2,24 +2,21 @@ import React from 'react';
 import './Information.css';
 
 export default function Information({ person, onClose }) {
-  const imageUrl = `https://starwars-visualguide.com/assets/img/characters/${person.index + 1}.jpg`;
+    const imageUrl = `https://starwars-visualguide.com/assets/img/characters/${person.index + 1}.jpg`;
 
-  return (
-    <section className="information">
-      <h2>{person.name}</h2>
-      <div className="container">
-        <div className="card">
-          <img src={imageUrl} alt={`Character: ${person.name}`} />
-          <h3>{person.name}</h3>
-          <p>Birth year: {person.birth_year}</p>
-          <p>Gender: {person.gender}</p>
-          <p>Hair color: {person.hair_color}</p>
-          <p>Height: {person.height}</p>
-          <p>Mass: {person.mass}</p>
-          <p>Skin color: {person.skin_color}</p>
-        </div>
-      </div>
-      <button onClick={onClose}>Fechar</button>
-    </section>
-  );
+    return (
+        <section className="information">
+            <div className="card">
+                <img src={imageUrl} alt={`Character: ${person.name}`} />
+                <h3>Name: {person.name}</h3>
+                <p>Birth year: {person.birth_year}</p>
+                <p>Gender: {person.gender}</p>
+                <p>Hair color: {person.hair_color}</p>
+                <p>Height: {person.height}</p>
+                <p>Mass: {person.mass}kg</p>
+                <p>Skin color: {person.skin_color}</p>
+                <button onClick={onClose}>Fechar</button>
+            </div>
+        </section>
+    );
 }
