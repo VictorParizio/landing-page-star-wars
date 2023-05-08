acf6295efa72bf6f83416d1ac1ef2e45102e9579
Victor Parizio <victorparizio@hotmail.com>
2023-05-08 09:07:04 -0300

    feat (Characters) Character Modal Foi adicionada a função "opemModal" ao botão information para abrir um modal com informações pessoais do personagem.

--- src/Component/Characters/Characters.jsx
+++ src/Component/Characters/Characters.jsx
@@ -1,30 +1,46 @@
 import React, { useState, useEffect } from 'react';
 import axios from "axios";
 import './Characters.css'
+import Information from '../Information/Information';
 
 export default function Characters() {
-    const [peoples, setPeople] = useState([]);
+    const [people, setPeople] = useState([]);
+    const [modalOpen, setModalOpen] = useState(false);
+
+    const openModal = () => {
+        setModalOpen(true);
+    };
+
+    const closeModal = () => {
+        setModalOpen(false);
+    };
 
     useEffect(() => {
         axios
-            .get('https://swapi.dev/api/people')
-            .then((response) => {
-                setPeople(response.data.results);
-            })
-    }, []);
+          .get('https://swapi.dev/api/people')
+          .then((response) => {
+            setPeople(response.data.results);
+            setLoading(false);
+          })
+          .catch((error) => {
+            console.error('Erro na chamada à API:', error);
+            setLoading(false);
+          });
+      }, []);
 
     return (
         <section className="characters">
             <h2 id="characters">Characters</h2>
             <div className="container">
-                {peoples.map((people, index) => (
+                {people.map((person, index) => (
                     <div className="card" key={index}>
-                        <img src={`https://starwars-visualguide.com/assets/img/characters/${[index + 1]}.jpg`} alt={`Character: ${people.title}`} />
-                        <h3>{people.name}</h3>
-                        <button>information!</button>
+                        <img src={`https://starwars-visualguide.com/assets/img/characters/${[index + 1]}.jpg`} alt={`Character: ${person.name}`} />
+                        <h3>{person.name}</h3>
+                        <button onClick={openModal}>information!</button>
                     </div>
                 ))}
             </div>
+            {modalOpen && <Information onClose={closeModal} />}
         </section >
     );
 }
